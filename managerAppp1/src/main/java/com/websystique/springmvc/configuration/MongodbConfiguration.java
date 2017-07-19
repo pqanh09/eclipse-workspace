76bfd90b6c77b22193f55a5bdb6f29421403c5dd
pqanh <pqanh09@gmail.com>
2017-07-20 05:47:02 +0700

    Add Manager AP 1 project

---
+++ managerAppp1/src/main/java/com/websystique/springmvc/configuration/MongodbConfiguration.java
@@ -0,0 +1,34 @@
+package com.websystique.springmvc.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.authentication.UserCredentials;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.mongodb.MongoClient;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan({ "com.websystique.springmvc.configuration" })
+@PropertySource(value = { "classpath:application.properties" })
+public class MongodbConfiguration {
+
+    @Bean
+    public MongoDbFactory mongoDbFactory() throws Exception {
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        UserCredentials userCredentials = new UserCredentials("", "");
+        return new SimpleMongoDbFactory(mongoClient, "websystique", userCredentials);
+    }
+ 
+    @Bean
+    public MongoTemplate mongoTemplate() throws Exception {
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
+        return mongoTemplate;
+    }
+}
+
